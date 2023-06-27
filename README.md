94f5b2dd8de06909f7b409b38a7ba5b66f23a5c3
Hubinnn <q490183462@163.com>
2023-06-27 15:09:33 +0800

    add pointproofs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Implementation
 Implementation of our DPSS protocol
+
+To use pointproofs, first clone `opDPSSTeam/pointproofs` and run `make build` to generate `target` files.
+Copy the `target` folder to `pkg` and rename it as `pointproofs_target`.
---
+++ pkg/pointproofs/pointproofs_test.go
@@ -0,0 +1,32 @@
+package pointproofs
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	kbls "github.com/kilic/bls12-381"
+)
+
+func TestVC(t *testing.T) {
+	var len int
+	for i := 1; i < 22; i++ {
+		len = 3*i + 1
+		var messages []kbls.Fr
+		for i := 0; i < len; i++ {
+			fr := kbls.Fr{}
+			if _, err := fr.Rand(rand.Reader); err != nil {
+				panic("")
+			}
+			fr.ToRed()
+			messages = append(messages, fr)
+		}
+		vc := New(len)
+		commitment := vc.Commit(messages)
+		witness := vc.Open(messages, 2)
+		if !vc.Verify(commitment, messages[2], 2, witness) {
+			panic("")
+		}
+		fmt.Printf("[len = %d] verify ok\n", len)
+	}
+}
